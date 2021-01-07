6f9b55da14795a81a29e6df474c23652230ff190
rubo <547789750@qq.com>
2021-01-07 21:44:09 +0800

    添加商户号

--- src/common/config/config.js
+++ src/common/config/config.js
@@ -2,8 +2,8 @@
 module.exports = {
   default_module: 'api',
   weixin: {
-    appid: '', // 小程序 appid
-    secret: '', // 小程序密钥
+    appid: 'wxccf4f7c38a2ed0d5', // 小程序 appid
+    secret: 'e322d6860ffda359f47f1b1da87a6b2b', // 小程序密钥
     mch_id: '', // 商户帐号ID
     partner_key: '', // 微信支付密钥
     notify_url: '' // 微信异步通知，例：https://www.nideshop.com/api/pay/notify
